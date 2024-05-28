9131955bf3946ebd6a60f760e04d5996d4fbc9e2
Leon <leontejeda@gmail.com>
2024-05-27 20:18:23 -0600

    Boton de Login

--- Tecnologias para la Web/2020/Proyecto/tailwind.config.js
+++ Tecnologias para la Web/2020/Proyecto/tailwind.config.js
@@ -57,3 +57,18 @@ module.exports = {
     }
   }
 }
+
+//para espaciado
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    }
+  }
+}
