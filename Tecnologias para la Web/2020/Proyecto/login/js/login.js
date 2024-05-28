4ec765123ef3dc2bf8777dc9ee94f37d3b73e629
Leon <leontejeda@gmail.com>
2024-05-28 17:24:52 -0600

    Instalación socket.io y modifiación del login

---
+++ Tecnologias para la Web/2020/Proyecto/login/js/login.js
@@ -0,0 +1,9 @@
+import Swal from 'sweetalert2'
+
+// or via CommonJS
+const Swal = require('sweetalert2')
+
+var exampleSocket = new WebSocket(
+    "http://127.0.0.1:5500/Tecnologias%20para%20la%20Web/2020/Proyecto/login/login",
+    "protocolOne",
+  );
