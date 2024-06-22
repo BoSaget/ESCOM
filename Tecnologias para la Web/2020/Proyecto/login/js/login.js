da5b93ca024ada6c1ef76e7b712753813cee8038
Leon <leontejeda@gmail.com>
2024-06-21 19:41:41 -0600

    Actualizacion de CSS

--- Tecnologias para la Web/2020/Proyecto/login/js/login.js
+++ Tecnologias para la Web/2020/Proyecto/login/js/login.js
@@ -1,9 +1,16 @@
-import Swal from 'sweetalert2'
-
-// or via CommonJS
-const Swal = require('sweetalert2')
-
-var exampleSocket = new WebSocket(
-    "http://127.0.0.1:5500/Tecnologias%20para%20la%20Web/2020/Proyecto/login/login",
-    "protocolOne",
-  );
+document.addEventListener('DOMContentLoaded', (event) => {
+    const socket = io();
+  
+    const form = document.getElementById('login');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      const formData = {
+        email: form.email.value
+      };
+  
+      socket.emit('formSubmit', formData);
+      console.log('Datos del formulario enviados:', formData);
+    });
+  });
+  
