b811cc46215539fd8a9186bc415fc55c898a8b29
Leon <leontejeda@gmail.com>
2024-06-21 22:10:58 -0600

    Cambios con usuario

--- Tecnologias para la Web/2020/Proyecto/usuario.js
+++ Tecnologias para la Web/2020/Proyecto/usuario.js
@@ -4,6 +4,23 @@ document.getElementById('user-menu-button').addEventListener('click', function()
     userMenu.classList.toggle('hidden');
 });
 
+document.getElementById('mobile-menu-button').addEventListener('click', function() 
+{
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
+
+// Optional: Close the dropdown when clicking outside of it
+window.addEventListener('click', function(event) 
+{
+    var mobileMenuButton = document.getElementById('mobile-menu-button');
+    var mobileMenu = document.getElementById('mobile-menu');
+    if (!mobileMenuButton.contains(event.target) && !mobileMenu.contains(event.target)) 
+    {
+        mobileMenu.classList.add('hidden');
+    }
+});
+
 /*
 // Optional: Close the dropdown when clicking outside of it
 window.addEventListener('click', function(event) 
