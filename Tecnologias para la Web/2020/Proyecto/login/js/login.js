348066e3a5ea5fe784bfe8012d97cb366775d9e0
Leon <leontejeda@gmail.com>
2024-06-22 23:42:48 -0600

    Borrado de server y uso de sweet alerts con filtros basicos

--- Tecnologias para la Web/2020/Proyecto/login/js/login.js
+++ Tecnologias para la Web/2020/Proyecto/login/js/login.js
@@ -1,17 +1,84 @@
-function sendData() {
-  const form = document.getElementById('login');
-  const formData = new FormData(form);
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('login-button').addEventListener('click', function(e) {
+    
+    e.preventDefault();
+    var formularioIngreso = document.getElementById('login');
+    var datosIngreso = new FormData(formularioIngreso);
 
-  fetch('login.php', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.text())
-  .then(data => {
-    console.log(formData.user-login);
-    document.getElementById('response').innerHTML = data;
-  })
-  .catch(error => {
-    console.error('Error:', error);
+    //Revisa si ambos campos se encuentran vacios
+    if(datosIngreso.get('user-login') == '' && datosIngreso.get('user-login-pass')=='')
+      {
+        Swal.fire({
+          title: '¡Cuidado!',
+          text: 'Ambos campos se encuentran vacios en login',
+          icon: 'warning',
+          confirmButtonText: 'Aceptar'
+        });
+      }
+
+    //Validation de si el usuario en registro está vacio
+    else if(datosIngreso.get('user-login') == '')
+    {
+      Swal.fire({
+        title: '¡Cuidado!',
+        text: 'Ingresa un usuario en login',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar'
+      });
+    }
+
+    //Validación para ver si el campo de contraseña está vacio
+    else if(datosIngreso.get('user-login-pass') == '')
+    {
+      Swal.fire({
+        title: '¡Cuidado!',
+        text: 'Ingresa una contraseña en login',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar'
+      });
+    }
+
   });
-}
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('sign-button').addEventListener('click', function(e) {
+    
+    e.preventDefault();
+    var formularioRegistro = document.getElementById('registro');
+    var datosRegistro = new FormData(formularioRegistro);
+
+    //Revisa si ambos campos se encuentran vacios
+    if(datosRegistro.get('user-sign') == '' && datosRegistro.get('user-sign-pass') == '')
+      {
+        Swal.fire({
+          title: '¡Cuidado!',
+          text: 'Ambos campos se encuentran vacios en registro',
+          icon: 'warning',
+          confirmButtonText: 'Aceptar'
+        });
+      }
+
+    //Validation de si el usuario en registro está vacio
+    else if(datosRegistro.get('user-sign') == '')
+    {
+      Swal.fire({
+        title: '¡Cuidado!',
+        text: 'Ingresa un usuario en registro',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar'
+      });
+    }
+
+    //Validación para ver si el campo de contraseña está vacio
+    else if(datosRegistro.get('user-sign-pass') == '')
+    {
+      Swal.fire({
+        title: '¡Cuidado!',
+        text: 'Ingresa una contraseña en registro',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar'
+      });
+    }
+  });
+});
