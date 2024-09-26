0576bfebbacbc9b784cec63dd1178ee6f4891576
Saget <leontejeda@gmail.com>
2024-09-25 23:38:08 -0600

    Primera Prueba de Spring con JAva

---
+++ Ingeniería de Software/pruebaSpring/src/test/java/ipn/mx/pruebaSpring/PruebaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ipn.mx.pruebaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
