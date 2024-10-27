72c15d6c3662060c02e2ae2c3c1788a43a40d45e
Saget <leontejeda@gmail.com>
2024-10-26 23:07:50 -0600

    Acomodo de carpeta y practica 2

---
+++ Desarrollo de Aplicaciones Moviles Nativas/Practica-2/src/test/java/ipn/mx/Practica2/Practica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ipn.mx.Practica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
